27cff45b370a52723f84d14b8ce561be53096562
joshitachu <joshikabel@gmail.com>
2023-10-19 13:14:39 +0200

    all the new code with node backend

--- app.js
+++ app.js
@@ -8,26 +8,34 @@ const app= ex();
 
 app.set("view engine", "ejs");
 app.set("views", "views"); // Set the views directory
+app.use(ex.static("public")); // Serve static assets like images
  
 app.listen(port);
 
 app.get("/", function(req,res){
     res.render("index");
+    console.log(req.url);
    
 });
 
 app.get("/form", function(req,res){
     res.render('form')
     console.log(req.url);
+
 })
 
 app.get("/about", function(req,res){
     res.render("about");
+    console.log(req.url);
+
     
+
+     
 });
 
 app.use(function(req,res){
-
     res.status(404).render("404");
+    console.log(req.url);
+
     
 });
